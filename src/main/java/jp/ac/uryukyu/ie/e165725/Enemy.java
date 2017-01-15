35742ea20ca00633f18b5cb1ac03cc19d5721f39
masakiyo okuhama <e165725@ie.u-ryukyu.ac.jp>
2017-01-15 10:50:40 +0900

    終わった

--- src/main/java/jp/ac/uryukyu/ie/e165725/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e165725/Enemy.java
@@ -7,5 +7,22 @@ public class Enemy extends LivingThing {
     public Enemy(String name, int maximumHP, int attack){
         super(name,maximumHP,attack);
     }
+    @Override
+    public void attack(LivingThing e){
+        if(!isDead()){
+            int damage = (int)(Math.random() * getAttack());
+            if (damage ==0){
+                System.out.printf("%sの攻撃！、、、だが、%sは攻撃を回避した。\n",getName(),e.getName());
+            }else {
+                int judge = (int) (Math.random() * 10);
+                if (judge <= 1) {
+                    System.out.printf("%sの攻撃！急所に当たった！%sに%dのダメージを与えた！\n", getName(), e.getName(), damage * 2);
+                } else {
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた。\n", getName(), e.getName(), damage);
+                    e.wounded(damage);
+                }
+            }
 
+        }
+    }
 }
--- src/main/java/jp/ac/uryukyu/ie/e165725/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e165725/Hero.java
@@ -8,4 +8,22 @@ public class Hero extends LivingThing {
     public Hero(String name, int maximumHP, int attack) {
         super(name,maximumHP, attack);
     }
+    @Override
+    public void attack(LivingThing e){
+        if(!isDead()){
+            int damage = (int)(Math.random() * getAttack());
+            if (damage ==0){
+                System.out.printf("%sの攻撃！、、、だが、%sは攻撃を回避した。\n",getName(),e.getName());
+            }else {
+                int judge = (int) (Math.random() * 10);
+                if (judge <= 2) {
+                    System.out.printf("%sの攻撃！急所に当たった！%sに%dのダメージを与えた！\n", getName(), e.getName(), damage * 2);
+                } else {
+                    System.out.printf("%sの攻撃！%sに%dのダメージを与えた。\n", getName(), e.getName(), damage);
+                    e.wounded(damage);
+                }
+            }
+
+        }
+    }
 }
